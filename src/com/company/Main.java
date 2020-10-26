3d04f0ed436150f4e4d88c0e29665b81a2691dab
LingDingLauPau <kasperjakob@me.com>
2020-10-26 11:19:46 +0100

    IntelliJ Java files
    print statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Regnemaskine regnemaskine = new Regnemaskine();
+
+        System.out.println(regnemaskine.add(2,2));
+        System.out.println(regnemaskine.subtract(2,8));
     }
 }
